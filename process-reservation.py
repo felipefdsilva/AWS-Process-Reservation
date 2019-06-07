8727a74711424539ce34e0cbe73d003844e4c73d
Felipe Ferreira <felipeferreira@poli.ufrj.br>
2019-06-07 19:05:38 -0300

    adds final version of process-reservation and deletes exchange_reservation.py and modify-reservation.py

--- process-reservation.py
+++ process-reservation.py
@@ -1,17 +1,57 @@
 #!/usr/bin/env python3
-# Description: Automatically modifies aws instance reservations
+# Description: Automatically modifies aws instance reservations (main function)
 # Author: Felipe Ferreira da Silva
 # Date: 27/05/2019
 
 import boto3 #for aws api
 import json #for pretty print
 import sys #for argv
+from modify import modify_reservation
+from exchange import exchange_reservation
 
-#This is the main function
-#Will receive
+if (len(sys.argv) != 5):
+    print ("Usage: python %s <reservation id> <instance count for new reservation> <target instance type> <max_price_difference>")
+    exit(1)
+
+reservation_id = sys.argv[1]
+target_instance_type = sys.argv[3]
+max_hourly_price_difference = float(sys.argv[4])
+ 
+#Instanciando cliente
+session = boto3.Session(profile_name = 'painel')
+client = session.client('ec2')
+
+#Coletando dados da reserva
+reservation_description = client.describe_reserved_instances(
+    ReservedInstancesIds = [reservation_id],
+)['ReservedInstances'][0]
 """
---original reservation ids, separated by commas
---list of instance count of each new reservation, separated by '-'
---target instance type
---max hourly price difference
+print ("Reservation Description")
+print (json.dumps(reservation_description, indent = 4, sort_keys = True, default = str))
+print ("---------- End of Description -------------")
 """
+new_instance_count = int(sys.argv[2])
+remaining_instance_count = int(reservation_description['InstanceCount'])-new_instance_count
+
+if (remaining_instance_count < 0):
+	print ("New instance count is greater than current")
+	exit(1)
+
+elif (remaining_instance_count == 0):
+	instance_count_list = [new_instance_count]
+	
+else:
+	instance_count_list = [new_instance_count, remaining_instance_count]
+
+	modification_response = modify_reservation(client, reservation_description, instance_count_list)
+	print ("New reservations after the modification:")
+	print ((json.dumps(modification_response['ModificationResults'], indent = 4, sort_keys = True, default = str)))
+
+	for reservation in modification_response['ModificationResults']:
+		if (reservation['TargetConfiguration']['InstanceCount'] == new_instance_count):
+			reservation_description = client.describe_reserved_instances(
+			    ReservedInstancesIds = [reservation['ReservedInstancesId']],
+			)['ReservedInstances'][0]
+
+print ("Exchanging reservation %s" %(reservation_description['ReservedInstancesId']))
+print (exchange_reservation(client, reservation_description, target_instance_type, max_hourly_price_difference))
